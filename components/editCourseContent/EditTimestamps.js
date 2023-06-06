dad9ee43b8d50e77950aa37f49b0bafb6e1e8a80
Chris Morrison <chrnatmor@gmail.com>
2023-06-06 17:03:01 -0400

    fix form css

--- components/editCourseContent/EditTimestamps.js
+++ components/editCourseContent/EditTimestamps.js
@@ -43,14 +43,17 @@ const AddCourseVideoContent = ({ onSubmit, documentIdOfVideo, courseCode }) => {
   const id = open ? "simple-popover" : undefined;
   const router = useRouter();
   documentIdOfVideo = router.query.docId;
-  console.log("Test: ",router.query.docId)
+  console.log("Test: ", router.query.docId);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const videoQuestionsCollection = collection(db, "videoQuestions");
         const querySnapshot = await getDocs(
-          query(videoQuestionsCollection, where("contentId", "==", documentIdOfVideo))
+          query(
+            videoQuestionsCollection,
+            where("contentId", "==", documentIdOfVideo)
+          )
         );
 
         const videoQuestionsData = [];
@@ -109,7 +112,7 @@ const AddCourseVideoContent = ({ onSubmit, documentIdOfVideo, courseCode }) => {
     console.log(updatedQuestions);
   };
 
-   const handleQuestionChange = (e, index) => {
+  const handleQuestionChange = (e, index) => {
     const updatedQuestions = [...questions];
     updatedQuestions[index].question = e.target.value;
     setQuestions(updatedQuestions);
@@ -123,7 +126,6 @@ const AddCourseVideoContent = ({ onSubmit, documentIdOfVideo, courseCode }) => {
     console.log(updatedQuestions);
   };
 
-
   const handleCorrectAnswerChange = (e, index, value) => {
     const updatedQuestions = [...questions];
     updatedQuestions[index].correctAnswer = value;
@@ -134,25 +136,27 @@ const AddCourseVideoContent = ({ onSubmit, documentIdOfVideo, courseCode }) => {
   const handleEditQuestion = async (index, docId) => {
     event.preventDefault();
     console.log("editing question at index", index);
-  
+
     const questionToEdit = questions[index];
     console.log(questionToEdit);
-    const { currentDocId, ...questionWithoutDocId } = questionToEdit;  
+    const { currentDocId, ...questionWithoutDocId } = questionToEdit;
     console.log("Edited doc: ", questionWithoutDocId);
-     
+
     try {
       // Update the specific question in the 'videoQuestions' collection
-      await updateDoc(doc(db, "videoQuestions", currentDocId), questionWithoutDocId);
+      await updateDoc(
+        doc(db, "videoQuestions", currentDocId),
+        questionWithoutDocId
+      );
       console.log("Question updated with ID: ", currentDocId);
-  
-      alert("Successfully updated question!")
-  
+
+      alert("Successfully updated question!");
+
       // Additional logic or navigation can be implemented here
     } catch (error) {
       console.error("Error updating question: ", error);
     }
   };
-  
 
   //////////// For Adding New Question ////////////
 
@@ -200,7 +204,7 @@ const AddCourseVideoContent = ({ onSubmit, documentIdOfVideo, courseCode }) => {
   const handleAddNewQuestion = async () => {
     event.preventDefault();
     console.log("adding new question");
-  
+
     console.log("Answers: ", newQuestionAnswers);
     console.log("Doc id:", documentIdOfVideo);
     const updatedQuestion = {
@@ -209,20 +213,23 @@ const AddCourseVideoContent = ({ onSubmit, documentIdOfVideo, courseCode }) => {
       correctAnswer: newQuestionCorrectAnswer,
       contentId: documentIdOfVideo,
     };
-  
+
     console.log(updatedQuestion);
-  
+
     try {
       // Add updatedQuestion to the 'videoQuestions' collection
-      const docRef = await addDoc(collection(db, "videoQuestions"), updatedQuestion);
+      const docRef = await addDoc(
+        collection(db, "videoQuestions"),
+        updatedQuestion
+      );
       console.log("New question added with ID: ", docRef.id);
-  
+
       // Reset state and perform any other necessary actions
       setQuestions((prevQuestions) => [...prevQuestions, updatedQuestion]);
       setNewQuestionAnswers([]);
       setNewQuestion({});
       setNewQuestionCorrectAnswer(null);
-  
+
       // Additional logic or navigation can be implemented here
     } catch (error) {
       console.error("Error adding new question: ", error);
@@ -231,183 +238,182 @@ const AddCourseVideoContent = ({ onSubmit, documentIdOfVideo, courseCode }) => {
 
   ////////////////////
 
-
-
-
   return (
     <>
-      
-    <div>
+      <div>
         {questions.map((question, index) => (
-        <form key={index} className="form-lg" onSubmit={() => handleEditQuestion(index, question.currentDocId)}>
-          <div className="existing-q-wrapper">
-          <label>Question {index + 1}</label>
-          <div className="flex flex-col">
-            <label className="sm">Hours : Minutes : Seconds</label>
-            <div className="flex align-center">
-              <input
-                onChange={(e) => handleHourChange(e, index)}
-                type="number"
-                name="minutes"
-                min="0"
-                max="10"
-                defaultValue={question.hour}
-                required
-                className="w-[5rem] inline"
-              />
-              <p
-                style={{
-                  fontSize: "24px",
-                  marginLeft: ".5rem",
-                  marginRight: ".5rem",
-                }}
-                className="text-black"
-              >
-                {" "}
-                :{" "}
-              </p>
-              <input
-                onChange={(e) => handleMinuteChange(e, index)}
-                type="number"
-                name="minutes"
-                min="0"
-                max="59"
-                defaultValue={question.minute}
-                required
-                className="w-[5rem] inline"
-              />
-              <p
-                style={{
-                  fontSize: "24px",
-                  marginLeft: ".5rem",
-                  marginRight: ".5rem",
-                }}
-                className="text-black"
-              >
-                {" "}
-                :{" "}
-              </p>
-              <input
-                onChange={(e) => handleSecondChange(e, index)}
-                type="number"
-                name="seconds"
-                min="0"
-                max="59"
-                defaultValue={question.second}
-                required
-                className="w-[5rem] inline"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <label className="sm">Question: </label>
-            <input
-              type="text"
-              defaultValue={question.question}
-              onChange={(e) => handleQuestionChange(e, index)}
-            ></input>
-          </div>
-          <div className="flex flex-row mt-5">
-            <div>
-              <label className="sm">Option 1</label>
-              <input
-                type="text"
-                placeholder="Option 1"
-                defaultValue={question.answers[0]}
-                onChange={(e) => handleAnswerChange(e, index, 0)}
-              />
-            </div>
-            <div>
-              <label className="sm">Option 2</label>
-              <input
-                type="text"
-                placeholder="Option 2"
-                defaultValue={question.answers[1]}
-                onChange={(e) => handleAnswerChange(e, index, 1)}
-              />
-            </div>
-            <div>
-              <label className="sm">Option 3</label>
-              <input
-                type="text"
-                placeholder="Option 3"
-                defaultValue={question.answers[2]}
-                onChange={(e) => handleAnswerChange(e, index, 2)}
-              />
-            </div>
-            <div>
-              <label className="sm">Option 4</label>
-              <input
-                type="text"
-                placeholder="Option 4"
-                defaultValue={question.answers[3]}
-                onChange={(e) => handleAnswerChange(e, index, 3)}
-              />
-            </div>
-          </div>
-          <div className="mt-5">
-            <label>Correct Option</label>
-            <div className="flex flex-row justify-evenly">
-              <div>
-                <label className="sm">
+          <form
+            key={index}
+            className="form-lg"
+            onSubmit={() => handleEditQuestion(index, question.currentDocId)}
+          >
+            <div className="existing-q-wrapper">
+              <label>Question {index + 1}</label>
+              <div className="flex flex-col">
+                <label className="sm">Hours : Minutes : Seconds</label>
+                <div className="flex align-center">
                   <input
-                    type="radio"
-                    name="radioGroup"
-                    value="option1"
-                    checked={question.correctAnswer === 0 ? "checked" : ""}
-                    onChange={(e) => handleCorrectAnswerChange(e, index, 0)}
-                  />{" "}
-                  Option 1
-                </label>
-              </div>
-              <div>
-                <label className="sm">
+                    onChange={(e) => handleHourChange(e, index)}
+                    type="number"
+                    name="minutes"
+                    min="0"
+                    max="10"
+                    defaultValue={question.hour}
+                    required
+                    className="w-[5rem] inline"
+                  />
+                  <p
+                    style={{
+                      fontSize: "24px",
+                      marginLeft: ".5rem",
+                      marginRight: ".5rem",
+                    }}
+                    className="text-black"
+                  >
+                    {" "}
+                    :{" "}
+                  </p>
                   <input
-                    type="radio"
-                    name="radioGroup"
-                    value="option2"
-                    checked={question.correctAnswer === 1 ? "checked" : ""}
-                    onChange={(e) => handleCorrectAnswerChange(e, index, 1)}
-                  />{" "}
-                  Option 2
-                </label>
-              </div>
-              <div>
-                <label className="sm">
+                    onChange={(e) => handleMinuteChange(e, index)}
+                    type="number"
+                    name="minutes"
+                    min="0"
+                    max="59"
+                    defaultValue={question.minute}
+                    required
+                    className="w-[5rem] inline"
+                  />
+                  <p
+                    style={{
+                      fontSize: "24px",
+                      marginLeft: ".5rem",
+                      marginRight: ".5rem",
+                    }}
+                    className="text-black"
+                  >
+                    {" "}
+                    :{" "}
+                  </p>
                   <input
-                    type="radio"
-                    name="radioGroup"
-                    value="option3"
-                    checked={question.correctAnswer === 2 ? "checked" : ""}
-                    onChange={(e) => handleCorrectAnswerChange(e, index, 2)}
-                  />{" "}
-                  Option 3
-                </label>
+                    onChange={(e) => handleSecondChange(e, index)}
+                    type="number"
+                    name="seconds"
+                    min="0"
+                    max="59"
+                    defaultValue={question.second}
+                    required
+                    className="w-[5rem] inline"
+                  />
+                </div>
               </div>
-              <div>
-                <label className="sm">
-                  <input
-                    type="radio"
-                    name="radioGroup"
-                    value="option4"
-                    checked={question.correctAnswer === 3 ? "checked" : ""}
-                    onChange={(e) => handleCorrectAnswerChange(e, index, 3)}
-                  />{" "}
-                  Option 4
-                </label>
+              <div className="flex flex-col">
+                <label className="sm">Question: </label>
+                <input
+                  type="text"
+                  defaultValue={question.question}
+                  onChange={(e) => handleQuestionChange(e, index)}
+                ></input>
               </div>
+              <div className="flex flex-row mt-5 gap-x-5">
+                <div className="flex flex-col">
+                  <label className="sm mr-2">Option 1</label>
+                  <input  className="w-40"
+                    type="text"
+                    placeholder="Option 1"
+                    defaultValue={question.answers[0]}
+                    onChange={(e) => handleAnswerChange(e, index, 0)}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label className="sm mr-2">Option 2</label>
+                  <input  className="w-40"
+                    type="text"
+                    placeholder="Option 2"
+                    defaultValue={question.answers[1]}
+                    onChange={(e) => handleAnswerChange(e, index, 1)}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label className="sm mr-2">Option 3</label>
+                  <input  className="w-40"
+                    type="text"
+                    placeholder="Option 3"
+                    defaultValue={question.answers[2]}
+                    onChange={(e) => handleAnswerChange(e, index, 2)}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label className="sm mr-2">Option 4</label>
+                  <input  className="w-40"
+                    type="text"
+                    placeholder="Option 4"
+                    defaultValue={question.answers[3]}
+                    onChange={(e) => handleAnswerChange(e, index, 3)}
+                  />
+                </div>
+              </div>
+              <div className="mt-5">
+                <label>Correct Option</label>
+                <div className="flex flex-row justify-evenly">
+                  <div>
+                    <label className="sm">
+                      <input 
+                        type="radio"
+                        name="radioGroup"
+                        value="option1"
+                        checked={question.correctAnswer === 0 ? "checked" : ""}
+                        onChange={(e) => handleCorrectAnswerChange(e, index, 0)}
+                      />{" "}
+                      Option 1
+                    </label>
+                  </div>
+                  <div>
+                    <label className="sm">
+                      <input
+                        type="radio"
+                        name="radioGroup"
+                        value="option2"
+                        checked={question.correctAnswer === 1 ? "checked" : ""}
+                        onChange={(e) => handleCorrectAnswerChange(e, index, 1)}
+                      />{" "}
+                      Option 2
+                    </label>
+                  </div>
+                  <div>
+                    <label className="sm">
+                      <input
+                        type="radio"
+                        name="radioGroup"
+                        value="option3"
+                        checked={question.correctAnswer === 2 ? "checked" : ""}
+                        onChange={(e) => handleCorrectAnswerChange(e, index, 2)}
+                      />{" "}
+                      Option 3
+                    </label>
+                  </div>
+                  <div>
+                    <label className="sm">
+                      <input
+                        type="radio"
+                        name="radioGroup"
+                        value="option4"
+                        checked={question.correctAnswer === 3 ? "checked" : ""}
+                        onChange={(e) => handleCorrectAnswerChange(e, index, 3)}
+                      />{" "}
+                      Option 4
+                    </label>
+                  </div>
+                </div>
+                <div className="mt-5"></div>
+              </div>
+              <Button className="btn" variant="contained" type="submit">
+                Edit Question
+              </Button>
             </div>
-            <div className="mt-5"></div>
-          </div>
-          <Button className="btn" variant="contained" type="submit">
-            Edit Question
-          </Button>
-        </div>
-         
-         
-         </form>
-     ))}   </div> 
-    
+          </form>
+        ))}{" "}
+      </div>
+
       <form className="form-lg" onSubmit={handleAddNewQuestion}>
         <div className="new-q-wrapper">
           <label>New Question</label>{" "}
@@ -475,34 +481,35 @@ const AddCourseVideoContent = ({ onSubmit, documentIdOfVideo, courseCode }) => {
               onChange={(e) => handleNewQuestionChange(e)}
             ></input>
           </div>
-          <div className="flex flex-row mt-5">
-            <div>
-              <label className="sm">Option 1</label>
+          <div className="flex flex-row mt-5 gap-x-5">
+            <div className="flex flex-col">
+              <label className="sm mr-2">Option 1</label>
               <input
+                className="w-40"
                 type="text"
                 placeholder="Option 1"
                 onChange={(e) => handleNewAnswerChange(e, 0)}
               />
             </div>
-            <div>
-              <label className="sm">Option 2</label>
-              <input
+            <div className="flex flex-col">
+              <label className="sm mr-2">Option 2</label>
+              <input  className="w-40"
                 type="text"
                 placeholder="Option 2"
                 onChange={(e) => handleNewAnswerChange(e, 1)}
               />
             </div>
-            <div>
-              <label className="sm">Option 3</label>
-              <input
+            <div className="flex flex-col">
+              <label className="sm mr-2">Option 3</label>
+              <input  className="w-40"
                 type="text"
                 placeholder="Option 3"
                 onChange={(e) => handleNewAnswerChange(e, 2)}
               />
             </div>
-            <div>
-              <label className="sm">Option 4</label>
-              <input
+            <div className="flex flex-col">
+              <label className="sm mr-2">Option 4</label>
+              <input  className="w-40"
                 type="text"
                 placeholder="Option 4"
                 onChange={(e) => handleNewAnswerChange(e, 3)}
@@ -529,7 +536,6 @@ const AddCourseVideoContent = ({ onSubmit, documentIdOfVideo, courseCode }) => {
                     type="radio"
                     name="radioGroup"
                     value="option2"
-                   
                     onChange={(e) => handleNewCorrectAnswerChange(e, 1)}
                   />{" "}
                   Option 2
