215976775ba414508f02677e08d354263d454dd5
John Vinh <john.michael.vinh2@gmail.com>
2023-06-21 09:53:55 -0400

    Update fetchUsers to use SWR

--- hooks/fetchUsers.js
+++ hooks/fetchUsers.js
@@ -1,29 +1,29 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
+import useSWR from 'swr'
 import { doc, getDoc, getDocs, collection, getFirestore } from 'firebase/firestore'
 import { useAuth } from '../context/AuthContext'
 import { db } from '../firebase'
 
+const fetcher = async (url) => {
+    const usersCollection = collection(db, 'users');
+    const usersSnapshot = await getDocs(usersCollection);
+    const usersData = usersSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    }));
+    return usersData;
+}
+
 export default function useFetchUsers() {
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-    const [users, setUsers] = useState([])
-
     const { currentUser } = useAuth()
+    const { data, error } = useSWR('/api/users', fetcher)
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const usersCollection = collection(db, 'users');
-          const usersSnapshot = await getDocs(usersCollection);
-          const usersData = usersSnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
-          setUsers(usersData);
-          setLoading(false);
-        };
-      
-        fetchData();
-    }, [])
-
-    return { loading, error, users, setUsers }
+    return {
+        users: data,
+        loading: !error && !data,
+        error: error,
+        setUsers: () => {
+            throw new Error('setUsers is not supported with SWR. To mutate data, consider using mutate from SWR')
+        }
+    }
 }
