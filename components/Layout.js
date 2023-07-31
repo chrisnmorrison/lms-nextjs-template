6fa5a0039c53c0c4788022c5ec7ae7256f932ca5
Chris Morrison <chrnatmor@gmail.com>
2023-07-31 13:27:36 -0400

    fix security leak

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import Footer from './Footer'
-import Header from './Header'
-import Sidebar from './Sidebar'
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Login from './Login';
+import { useAuth } from '../context/AuthContext';
+import AccessDenied from './AccessDenied';
 
 export default function Layout(props) {
-    const { children } = props
-    return (
-        <div className='flex flex-col min-h-screen relative bg-neutral-800 text-white'> 
-        <Header/><div className='sidebar-and-main'> <Sidebar/>
+  const { children } = props;
+  const {currentUser, isAdmin} = useAuth();
+
+
+  return (
+    <div className='flex flex-col min-h-screen relative bg-neutral-800 text-white'> 
+      <Header />
+      <div className='sidebar-and-main'>
+        <Sidebar />
         <main className='flex-1 flex flex-col p-4 inline-block max-w-[82vw] ml-auto'>
-            {children}
-        </main></div>
-       
-        <Footer/>
-        </div>
-    )
+          {!currentUser && <Login />}
+          {currentUser && isAdmin ? (
+            <>
+              {children}
+            </>
+          ) : currentUser && !isAdmin ? (
+            <AccessDenied />
+          ) : null}
+        </main>
+      </div>
+      <Footer />
+    </div>
+  );
 }
